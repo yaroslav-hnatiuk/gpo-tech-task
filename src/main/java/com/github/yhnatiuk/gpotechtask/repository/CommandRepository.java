7aba8b27f64260137817de496768bd9f5497131a
yroslav <yaroslavfor@gmail.com>
2021-10-29 23:17:35 +0300

    Removed unnecessary annotation.

--- src/main/java/com/github/yhnatiuk/gpotechtask/repository/CommandRepository.java
+++ src/main/java/com/github/yhnatiuk/gpotechtask/repository/CommandRepository.java
@@ -8,9 +8,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommandRepository extends CrudRepository<Command, Long> {
 
   String querySetStatusFailedIfCommandWasExpired = "UPDATE command "
